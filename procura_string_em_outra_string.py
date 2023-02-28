090c74dbd4d43c7377209a3274f5c6325a0ccdac
Alberto Talayer <54676308+CarlosTalayer@users.noreply.github.com>
2023-02-28 12:25:45 -0300

    Create procura_string_em_outra_string.py
    Procura string dentro de outra string.

---
+++ procura_string_em_outra_string.py
@@ -0,0 +1,4 @@
+# Crie um programa que procure uma string dentro de outra.
+
+nome = str(input('Qual é seu nome completo? ')).strip() # .strip elimina quaisquer espaços que tiver no nome digitado.
+print = input('Seu nome tem Silva? {}' .format('SILVA' in nome.upper()))
