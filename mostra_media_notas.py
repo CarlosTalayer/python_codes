00c9185726a0e8914989daed2dafaaa1958da0b8
Alberto Talayer <54676308+CarlosTalayer@users.noreply.github.com>
2023-02-28 12:04:29 -0300

    Create mostra_media_notas.py
    CALCULA SOMA DE DUAS NOTAS DIGITADAS E MOSTRA A MÉDIA

---
+++ mostra_media_notas.py
@@ -0,0 +1,5 @@
+### CALCULA SOMA DE DUAS NOTAS DIGITADAS E MOSTRA A MÉDIA
+nota1 = int(input('Digite a primeira nota do aluno: '))
+nota2 = int(input('Agora insira a segunda nota: '))
+media = (nota1 + nota2) / 2
+print('A média das duas notas digitadas é {}.' .format(media))
