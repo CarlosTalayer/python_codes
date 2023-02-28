1d42b7f8c6b6fc9cc6714255e9140a72ee1a156a
Alberto Talayer <54676308+CarlosTalayer@users.noreply.github.com>
2023-02-28 12:01:50 -0300

    Create cria_numero_antecessor_e_sucessor.py
    Mostra número sucessor e antecessor

---
+++ cria_numero_antecessor_e_sucessor.py
@@ -0,0 +1,6 @@
+### N° SUCESSOR E ANTECESSOR
+n1 = int(input('Escreva um número inteiro: '))
+n2 = n1 + 1
+n3 = n1 - 1
+print('Os valores são os seguintes: \n o sucessor do primeiro número digitado é {} e seu antecessor é o número {}.' .format(n2, n3))
+# o valor \n faz com que a linha seja quebrada
