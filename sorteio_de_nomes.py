5dd7f11993c18978cbc8ecae13d97f6fa665cf26
Alberto Talayer <54676308+CarlosTalayer@users.noreply.github.com>
2023-02-28 12:20:26 -0300

    Create sorteio_de_nomes.py
    Código que pede a pessoa para digitar um nome, e depois o próprio programa escolhe um dos nomes escolhidos.

---
+++ sorteio_de_nomes.py
@@ -0,0 +1,13 @@
+# Código que pede a pessoa para digitar um nome, e depois o próprio programa escolhe um dos nomes escolhidos.
+from random import choice
+
+print('Sorteio do aluno que irá apagar o quadro.')
+print('-' * 50) # Aplica 50 hífens para dar um grau
+lista = [] #Lista começa vazia
+for c in range(1, 5): # define o n° de alunos
+    aluno = input(f'Digite o nome do {c}° aluno: ')
+    lista. append(aluno) # Adiciona cada um dos alunos digitados ou informados na lista.
+aluno_sorteado = choice(lista)
+print('O aluno sorteado para apagar o quadro é: {}.' .format(aluno_sorteado))
+#ou substitua por o código print abaixo
+#print(f'O aluno sorteado é: {aluno_sorteado}.')
